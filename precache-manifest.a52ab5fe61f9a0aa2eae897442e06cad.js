636255c233b0af9f9f1f21715ba1f700209be9b1
keyeypee <keyeypee@gmail.com>
2020-05-11 01:38:50 +0530

    Updates

---
+++ precache-manifest.a52ab5fe61f9a0aa2eae897442e06cad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2408b0fa7f1e204b26d129bf468f1f64",
+    "url": "/postal-codes-locator/index.html"
+  },
+  {
+    "revision": "9ba5f6f9613d25b100ec",
+    "url": "/postal-codes-locator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "092b7e125cf3ae19e42d",
+    "url": "/postal-codes-locator/static/js/2.ecc9952e.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/postal-codes-locator/static/js/2.ecc9952e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba5f6f9613d25b100ec",
+    "url": "/postal-codes-locator/static/js/main.997d06fd.chunk.js"
+  },
+  {
+    "revision": "8ca2b4e08b60f6919a02",
+    "url": "/postal-codes-locator/static/js/runtime-main.7c05c6a6.js"
+  }
+]);
